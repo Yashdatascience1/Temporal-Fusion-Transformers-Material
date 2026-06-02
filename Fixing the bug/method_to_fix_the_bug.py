8b3766a020c1f16e7040486f3a99a21d845ec9d4
Yash Saxena <yash.saxena0546@gmail.com>
2026-06-02 10:28:33 +0530

    added

--- Fixing the bug/method_to_fix_the_bug.py
+++ Fixing the bug/method_to_fix_the_bug.py
@@ -6,3 +6,20 @@ print(jj.groupby(jj.index.year).agg(
     MARRIAGE = ('MARRIAGE_DAYS', 'sum'),
     NET_SALES= ('NET_SALES', 'sum'),
 ))
+
+import matplotlib.pyplot as plt
+
+explainer.plot_variable_selection(result)
+
+fig = plt.gcf()
+fig.set_size_inches(18, 12)
+
+for ax in fig.get_axes():
+    for label in ax.get_xticklabels():
+        label.set_rotation(45)
+        label.set_horizontalalignment('right')
+    ax.tick_params(axis='both', labelsize=11)
+    ax.title.set_fontsize(13)
+
+fig.tight_layout()
+plt.show()
