03178fe7165e5774e55dcf7496977c4947f39f52
Yash Saxena <yash.saxena0546@gmail.com>
2026-07-20 21:28:13 +0530

    added new file

---
+++ Daily Sales Forecasting Methodology/Modelling code/deleting_dataframes.py
@@ -0,0 +1,4 @@
+del train_targets_stacked, train_sales, train_flags, train_sales_scaled
+del val_targets_stacked, val_sales_aligned, val_sales_scaled_aligned
+del _val_present, _val_present_transformed, scaled_val_targets
+gc.collect()
